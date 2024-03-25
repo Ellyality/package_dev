11aeeb000271f40dac030ca0051d927e9edab08e
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2024-03-25 09:26:49 +0800

    Update git clone repo list, increase for fork repo list

--- clone_packages.sh
+++ clone_packages.sh
@@ -15,4 +15,19 @@ git clone https://github.com/Ellyality/package_aeroUI.git
 git clone https://github.com/Ellyality/package_unity_free_camera.git
 git clone https://github.com/Ellyality/package_xlua.git
 git clone https://github.com/Ellyality/package_unibus.git
+git clone https://github.com/Ellyality/package_crest.git
+git clone https://github.com/Ellyality/package_ffmpeg_bind2.git
+git clone https://github.com/Ellyality/package_particle_effect2D.git
+git clone https://github.com/Ellyality/package_volumetric_lights.git
+git clone https://github.com/Ellyality/package_bulletsharp.git
+git clone https://github.com/Ellyality/package_delaunay.git
+git clone https://github.com/Ellyality/package_heap_explorer.git
+git clone https://github.com/Ellyality/package_uniGif.git
+git clone https://github.com/Ellyality/package_custom_toolbar.git
+git clone https://github.com/Ellyality/package_smart_inspector.git
+git clone https://github.com/Ellyality/package_smart_hierarchy.git
+git clone https://github.com/Ellyality/package_road_architect.git
+git clone https://github.com/Ellyality/package_http.git
+git clone https://github.com/Ellyality/package_hex_tiles.git
+git clone https://github.com/Ellyality/package_pixel_camera.git
 pause
