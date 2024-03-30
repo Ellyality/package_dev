d3ba41433bb7a997878359ebd7934f2d4007347c
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-03-30 15:36:27 +0800

    update scripts

---
+++ Scripts/clone_package3.sh
@@ -0,0 +1,14 @@
+cd ../Assets
+git clone https://github.com/Ellyality/package_http.git
+git clone https://github.com/Ellyality/package_hex_tiles.git
+git clone https://github.com/Ellyality/package_pixel_camera.git
+git clone https://github.com/Ellyality/package_open_scenes.git
+git clone https://github.com/Elly2018/package_book.git
+git clone https://github.com/Ellyality/package_ingame_Debug_Console.git
+git clone https://github.com/Ellyality/package_next_gen_sprites.git
+git clone https://github.com/Ellyality/package_camera_shake.git
+git clone https://github.com/Ellyality/package_graphy.git
+git clone https://github.com/Ellyality/package_gpu_particles.git
+git clone https://github.com/Ellyality/package_unity_audio_spectrum.git
+git clone https://github.com/Ellyality/package_quicksheet.git
+pause
---
+++ Scripts/clone_packages.sh
@@ -0,0 +1,16 @@
+cd ../Assets
+git clone https://github.com/Ellyality/package_naughty_attributes.git
+git clone https://github.com/Ellyality/package_randomness.git
+git clone https://github.com/Ellyality/package_mapping.git
+git clone https://github.com/Ellyality/package_display.git
+git clone https://github.com/Ellyality/package_litenetlib.git
+git clone https://github.com/Ellyality/package_multiplayer_core.git
+git clone https://github.com/Ellyality/package_sqlite.git
+git clone https://github.com/Ellyality/package_socketio.git
+git clone https://github.com/Ellyality/package_utility.git
+git clone https://github.com/Ellyality/package_character.git
+git clone https://github.com/Ellyality/package_sdf.git
+git clone https://github.com/Ellyality/package_smoke.git
+git clone https://github.com/Ellyality/package_aeroUI.git
+git clone https://github.com/Ellyality/package_unity_free_camera.git
+pause
