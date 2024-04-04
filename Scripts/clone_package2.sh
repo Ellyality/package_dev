57c851f416ec81237e8a7678579d0600d56bc2d9
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-04-04 20:13:50 +0800

    update scripts

--- Scripts/clone_package2.sh
+++ Scripts/clone_package2.sh
@@ -2,7 +2,7 @@ cd ../Assets
 git clone https://github.com/Ellyality/package_xlua.git
 git clone https://github.com/Ellyality/package_unibus.git
 git clone https://github.com/Ellyality/package_crest.git
-git clone https://github.com/Ellyality/package_ffmpeg_bind2.git
+# git clone https://github.com/Ellyality/package_ffmpeg_bind2.git
 git clone https://github.com/Ellyality/package_particle_effect2D.git
 git clone https://github.com/Ellyality/package_volumetric_lights.git
 git clone https://github.com/Ellyality/package_bulletsharp.git
