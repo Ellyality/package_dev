e39a6e94dc029040343b062c37d4581672e21b9d
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-04-05 20:38:35 +0800

    Added package

---
+++ Scripts/clone_package5.sh
@@ -0,0 +1,28 @@
+cd ../Assets
+git clone https://github.com/Ellyality/package_univrm.git
+git clone https://github.com/Ellyality/package_toon_lit_shader.git
+git clone https://github.com/Ellyality/package_skybox_two_colors.git
+git clone https://github.com/Ellyality/package_r3.git
+git clone https://github.com/Ellyality/package_uI_effect.git
+git clone https://github.com/Ellyality/package_unitask.git
+git clone https://github.com/Ellyality/package_ultimate_outline.git
+git clone https://github.com/Ellyality/package_wireframe.git
+git clone https://github.com/Ellyality/package_3d_voxel.git
+git clone https://github.com/Ellyality/package_npr_lab.git
+git clone https://github.com/Ellyality/package_sonarFx.git
+git clone https://github.com/Ellyality/package_vertical_dissolve.git
+git clone https://github.com/Ellyality/package_sphere_dissolve.git
+git clone https://github.com/Ellyality/package_symbol.git
+git clone https://github.com/Ellyality/package_grapple_Effect.git
+git clone https://github.com/Ellyality/package_deform.git
+git clone https://github.com/Ellyality/package_procedural_flower.git
+git clone https://github.com/Ellyality/package_urfairy.git
+git clone https://github.com/Ellyality/package_xasset.git
+git clone https://github.com/Ellyality/package_uniBpm_analyzer.git
+git clone https://github.com/Ellyality/package_xCharts.git
+git clone https://github.com/Ellyality/package_gaussian_splatting.git
+git clone https://github.com/Ellyality/package_jtrp.git
+git clone https://github.com/Ellyality/package_shaderman.git
+git clone https://github.com/Ellyality/package_glTFast.git
+git clone https://github.com/Ellyality/package_radial_progressbar.git
+pause
