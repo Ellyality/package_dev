3f401b3b5c702318ed0e1efe38cdb10ba36b3187
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-04-05 15:31:13 +0800

    Update dev content

---
+++ Scripts/clone_package4.sh
@@ -0,0 +1,8 @@
+cd ../Assets
+git clone https://github.com/Ellyality/package_quicksheet.git
+git clone https://github.com/Ellyality/package_kino.git
+git clone https://github.com/Ellyality/package_webgl_template.git
+git clone https://github.com/Ellyality/package_bitmap_drawing.git
+git clone https://github.com/Ellyality/package_linq.git
+git clone https://github.com/Ellyality/package_bones3.git
+pause
