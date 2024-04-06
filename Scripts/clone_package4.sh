ed73b29033a0717b170342974d4516c309a73e70
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-04-06 16:12:38 +0800

    update script

--- Scripts/clone_package4.sh
+++ Scripts/clone_package4.sh
@@ -4,5 +4,5 @@ git clone https://github.com/Ellyality/package_kino.git
 git clone https://github.com/Ellyality/package_webgl_template.git
 git clone https://github.com/Ellyality/package_bitmap_drawing.git
 git clone https://github.com/Ellyality/package_linq.git
-git clone https://github.com/Ellyality/package_bones3.git
+git clone https://github.com/Ellyality/package_nuget.git
 pause
